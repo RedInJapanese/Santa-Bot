e0cba8cd7f56d2415602cbc58be2851bc43f9b43
RedInJapanese <abasu924@gmail.com>
2022-12-16 21:12:54 -0800

    add source code

---
+++ src/responses.py
@@ -0,0 +1,9 @@
+import random
+
+def handle_response(message):
+    asdf = message.lower();
+    
+    if(asdf == 'hello'):
+        return 'when the pizza rolls are done!'
+    else:
+        return "cringe!!!!"
