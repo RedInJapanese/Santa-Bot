06f4db4c3a1f274b7c2831ab8106beacc82315fb
RedInJapanese <abasu924@gmail.com>
2022-12-17 22:16:00 -0800

    done using the bot

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,8 @@
 import discord
 import responses
+import random
 
-async def send_message(message, user_message, is_private):
+async def send_m(message, user_message, is_private):
     try: 
         response = responses.handle_response(user_message)
         await message.author.send(response) if is_private else  await message.channel.send(response)
@@ -9,11 +10,12 @@ async def send_message(message, user_message, is_private):
         print(e)
 
 def run_discord_bot():
-    TOKEN = 'MTA1MzUxMzYyNzU2NDUxOTUyNQ.GpCldN.fiY8jpaMzOUPxHDr0n_JGBMveXBkKjMHMFOnYc'
+    TOKEN = ''
     
     
     intents = discord.Intents.default()
     intents.message_content = True
+    intents.members = True
     client = discord.Client(intents=intents)
 
     @client.event
@@ -29,11 +31,73 @@ def run_discord_bot():
         user_message = str(message.content)
         channel = str(message.channel)
 
-        print(f'{username} said: "{user_message}" ({channel})')
+        #print(f'{username} said: "{user_message}" ({channel})')
+        
+        # message = 'cringe!!!'
+        # channel = await member.create_dm()
+        # await channel.send(message)
+        #preeb address
+        #mine
 
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
+        l = ["Akash", "Ari", "Moz","Chat", "Pat",  "Preeb", "Geeg"]
+        og = ["Akash", "Ari", "Moz","Chat", "Pat",  "Preeb", "Geeg"]
+        uid = [278999817575727104, 213750170389708801, 152215478348021761, 240977124024909825, 181245170715590657, 352170758019088384, 178990313929441281]
+
+        uid_fake = [308370253266812928, 308370253266812928, 308370253266812928, 308370253266812928, 308370253266812928, 308370253266812928, 308370253266812928]
+
+        # santa = "Akash"
+        # santa = randomize(santa, "Akash", l)
+        # user = client.get_user(308370253266812928)
+        # message = "Rules: no wishlists, no dildos, no drugs, no alcohol, no sex toys, no telling people who you got, no asking for rerolls\n\n Your secret santa assginment is: " 
+        # message+=santa
+        # embed = discord.Embed(title="🅱 Secret Santa Assignment 🎅(Don't tell anyone who you get 🤫)", description=message)
+        # await user.send(embed=embed)
+
+        for i in range(0, len(uid)):
+            santa = og[i]
+            fin = ""
+            fin+=santa
+            fin+=" "
+            santa = randomize(santa, santa, l)
+            fin+=santa
+            #print(fin)
+            user = client.get_user(uid[i])
+            message = "Rules: no wishlists, no dildos, no drugs, no alcohol, no sex toys, no telling people who you got, no asking for rerolls\n\n Gift exchange will happen on the 30th. If your secret santa isn't able to make it, their address has been specified in this message(except for moz)\n\n Your secret santa assginment is: " 
+            message+=santa
+            embed = discord.Embed(title="🅱 Secret Santa Assignment 🎅(Don't tell anyone who you get 🤫)", description=message)
+            await user.send(embed=embed)
+
+        # #ari
+        # user = client.get_user(213750170389708801)
+        # await user.send('cringe!!!')
+        
+        # #moz
+        # user = client.get_user(152215478348021761)
+        # await user.send('cringe!!!')
+        
+        # #chat
+        # user = client.get_user(240977124024909825)
+        # await user.send('cringe!!!')
+
+
+        # #pat
+        # user = client.get_user(181245170715590657)
+        # await user.send('cringe!!!')
+
+        # #preeb
+        # user = client.get_user(352170758019088384)
+        # await user.send('cringe!!!')
+
+        # #geeg
+        # user = client.get_user(178990313929441281)
+        # await user.send('cringe!!!')
     client.run(TOKEN)
+
+def randomize(santa, name, l):
+    while(santa == name):
+        rand = random.randint(0, len(l)-1)
+        santa = l[rand]
+    l.remove(santa)
+    if(santa == "Preeb"):
+        santa += "(his address is 1140 W Blaine St, Riverside, CA 92507)"
+    return santa
--- src/responses.py
+++ src/responses.py
@@ -2,8 +2,8 @@ import random
 
 def handle_response(message):
     asdf = message.lower();
-    
-    if(asdf == 'hello'):
-        return 'when the pizza rolls are done!'
+
+    if(message == 'reminder'):
+        return "@here Just a reminder that the deadline for secret santa is December 30th! Don't late, or I'll have sex with your mother!"
     else:
-        return "cringe!!!!"
+        return "@here Everyone has been sent their respective secret santa assignments. If there is a mistake or you didn't get your assignment for whatever reason, please DM Akash."
